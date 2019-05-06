392c97cd4db14d01534533d2723e08f47df5ac4a
toren332 <toren332@gmail.com>
2019-05-06 04:58:32 +0300

    jsoneditor

---
+++ JSONEDITOR/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework import routers
+from editor import views as editor_views
+
+router = routers.DefaultRouter()
+router.register('json', viewset=editor_views.JsonViewSet, basename='json')
+urlpatterns = router.urls
+urlpatterns += path('admin/', admin.site.urls),
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
